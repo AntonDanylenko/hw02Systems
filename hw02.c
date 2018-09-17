335b09b6057416c0aefba5efa0b18da7693dddc9
AntonDanylenko <adanylenko@stuy.edu>
2018-09-16 22:19:33 -0400

    prints each individual byte of int

--- hw02.c
+++ hw02.c
@@ -1,5 +1,10 @@
+#include <stdio.h>
 int main(){
   unsigned int x = 3000000000;
-  char * address = &x;
-  printf("Int printed as hex: %x\n", address);
+  char * px = &x;
+  printf("Int printed as hex: %x\n", px);
+  for(int n=0; n<sizeof(x); n++){
+    printf("%d: %hhx", n, px)
+    px++;
+  }
 }
