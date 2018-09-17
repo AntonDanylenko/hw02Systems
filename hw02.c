0644bc43470af93cce1995f3ef78cb1f1131d470
AntonDanylenko <adanylenko@stuy.edu>
2018-09-16 22:32:49 -0400

    Incremented by 1 and 16

--- hw02.c
+++ hw02.c
@@ -7,4 +7,22 @@ int main(){
     printf("%d: %hhx", n, px)
     px++;
   }
+
+  px = &x;
+  for(int n=0; n<sizeof(x); n++){
+    *px++;
+    px++;
+  }
+  printf("Incrementing by 1.")
+  printf("Decimal value: %d\n", x);
+  printf("Hex value: %x\n", x);
+
+  px = &x;
+  for(int n=0; n<sizeof(x); n++){
+    *px+=16;
+    px++;
+  }
+  printf("Incrementing by 16.")
+  printf("Decimal value: %d\n", x);
+  printf("Hex value: %x\n", x);
 }
